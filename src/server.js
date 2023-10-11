539a0c6778b826b7b9e5c0050a3551fb864bdf27
윤영미 <maie421@naver.com>
2023-10-11 19:06:59 +0900

    http -> 소켓으로 변경

--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 import * as http from "http";
 import path from "path";
-import { Server } from "socket.io";
+// import { Server } from "socket.io";
 import express from "express";
+import { WebSocketServer } from 'ws';
+import url from "url"; // Import WebSocket
 
 const app = express();
 const __dirname = path.resolve();
@@ -12,8 +14,10 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const httpServer = http.createServer(app);
-const wsServer = new Server(httpServer);
+const server = http.createServer(app).listen(3000, () => {
+    console.log('https://localhost:3000/');
+});
+const wsServer = new WebSocketServer({server});
 
 // 웹 소켓 서버에서 연결 이벤트를 처리하는 부분
 wsServer.on("connection", (socket) => {
@@ -33,5 +37,5 @@ wsServer.on("connection", (socket) => {
   });
 });
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-httpServer.listen(3000, handleListen);
+// const handleListen = () => console.log(`Listening on http://localhost:3000`);
+// httpServer.listen(3000, handleListen);
