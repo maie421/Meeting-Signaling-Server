830883671296c2b58e2f97dd85eb77d677226354
윤영미 <maie421@naver.com>
2023-10-11 20:15:10 +0900

    Revert "http -> 소켓으로 변경"
    This reverts commit 539a0c6778b826b7b9e5c0050a3551fb864bdf27.

--- src/server.js
+++ src/server.js
@@ -1,13 +1,7 @@
+import * as http from "http";
 import path from "path";
+import { Server } from "socket.io";
 import express from "express";
-import { WebSocketServer } from 'ws';
-import * as https from "https";
-import fs from "fs"; // Import WebSocket
-
-const options = {
-  key: fs.readFileSync('../keys/server.key'),
-  cert: fs.readFileSync('../keys/server.crt')
-};
 
 const app = express();
 const __dirname = path.resolve();
@@ -18,45 +12,26 @@ app.use("/public", express.static(__dirname + "/public"));
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-let server = https.createServer(options, app).listen(3000, () => {
-  console.log('https://localhost:3000/');
-});
-
-
-const wsServer = new WebSocketServer({server});
+const httpServer = http.createServer(app);
+const wsServer = new Server(httpServer);
 
 // 웹 소켓 서버에서 연결 이벤트를 처리하는 부분
-wsServer.on('connection', ws => {
-  ws.on('error', error => {
-    console.error(`Connection %s error : %s`, ws.id, error);
-  });
-
-  ws.on('disconnect', data => {
-    console.log(`Connection : %s disconnect`, data);
-  });
-  ws.on('message', _message => {
-    let message = JSON.parse(_message);
-    console.log(`Connection: %s id`, message.id);
-
-    switch (message.id) {
-      case 'join_room':
-        console.log("join_room"+ message);
-        break;
-      case 'offer': //보낸 사람으로 부터 비디오 수신
-        console.log("offer"+ message);
-        break;
-      case 'answer':
-        console.log("answer"+ message);
-        break;
-      case 'leave':
-        console.log("leave"+ message);
-        break;
-      case 'ice':
-        console.log("ice"+ message);
-        break;
-    }
-  });
+wsServer.on("connection", (socket) => {
+    //방 참가
+    socket.on("join_room", (roomName) => {
+        socket.join(roomName);
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("offer", (offer, roomName) => {
+        socket.to(roomName).emit("offer", offer);
+    });
+    socket.on("answer", (answer, roomName) => {
+        socket.to(roomName).emit("answer", answer);
+    });
+    socket.on("ice", (ice, roomName) => {
+        socket.to(roomName).emit("ice", ice);
+    });
 });
 
-// const handleListen = () => console.log(`Listening on http://localhost:3000`);
-// httpServer.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
